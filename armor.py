b1ed2ae8c3092b9ac52bdb1a74913cd893572eee
sree-chikati <srechi453.fusdk12@gmail.com>
2020-11-15 16:08:44 -0800

    STEP 3: BUILD ABILITY AND ARMOR CLASSES

---
+++ armor.py
@@ -0,0 +1,19 @@
+import random
+
+class Armor:
+    def __init__(self, name, max_block):
+        '''
+        Initialize the values passed into this method as instance variables.
+        '''
+        self.name = name
+        self.max_block = max_block
+
+    def block(self):
+        random_block_value = random.randint(0, self.max_block)
+        return random_block_value
+
+if __name__ == "__main__":
+    # If you run this file from the terminal this block is executed.
+    armor = Armor("Debugging Shield", 10)
+    print(armor.name)
+    print(armor.block())
